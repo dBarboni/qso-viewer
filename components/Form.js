04514129f4d0670fa2f48e6739a76c9b9e606927
Dan <dbarboni07@gmail.com>
2021-08-21 11:08:38 -0400

    Add basic data retrieval logic

--- components/Form.js
+++ components/Form.js
@@ -1,13 +1,37 @@
 // Form component
 import styles from '../styles/Form.module.css'
+import axios from 'axios';
 
 export default function Form() {
     const retrieveData = e => {
         // Prevent refresh
         e.preventDefault();
-        // Call LOTW API
+
+        // Build query
         // Bare minimum needs login, password, qso_query params
-        // qso_startdate not req'd but recommended
+        // qso_startdate not req'd
+
+        // Define base params
+        const baseURL = 'https://lotw.arrl.org/lotwuser/lotwreport.adi';
+        let params = {
+            qso_query: 1,
+            login: e.target.call.value,
+            password: e.target.password.value
+        };
+
+        // Add date param if not empty
+        const startDate = e.target.date.value;
+        if (startDate !== '') {
+            params.qso_startdate = startDate;
+        }
+
+        // Call LOTW API
+        axios.get(baseURL, params)
+        .then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        });
     }
     return (
         <form onSubmit={retrieveData} className={styles.form}>

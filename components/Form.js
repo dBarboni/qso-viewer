b194972b5264482c7b18288575e193b53e52c694
Dan <dbarboni07@gmail.com>
2021-08-22 11:32:30 -0400

    Finish adif formatter function

--- components/Form.js
+++ components/Form.js
@@ -32,18 +32,24 @@ export default function Form() {
         .then(response => {
             //console.log(response.data);
             // if <eoh> tag exists then succesfully got data
-            if (response.data.indexOf("<eoh>") !== -1) {
-                const adifString = response.data;
+            const adifString = response.data;
+            if (adifString.indexOf('<eoh>') !== -1) {
                 const firstItem = adifString.indexOf('<CALL');
                 if (firstItem !== -1) {
                     // at least one record exists
                     // format adif and parse to json
                     let cleaned = adifString.slice(firstItem); // remove the header info
-                    cleaned = cleaned.replaceAll(/\/{2}.*/g, ""); // remove comments
-                    cleaned = cleaned.replaceAll(/\s+$/gm, ""); // trim blank space at EOL
-                    cleaned = cleaned.replaceAll(/\<(.*):\d+\>(.*)/g, "'$1':'$2'"); // format tags
-                    cleaned = cleaned.replace("<APP_LoTW_EOF>", ""); // remove EOF tag
-                    console.log(cleaned);
+                    cleaned = cleaned.replaceAll(/\/{2}.*/g, ''); // remove comments
+                    cleaned = cleaned.replaceAll(/\s+$/gm, ''); // trim blank space at EOL
+                    cleaned = cleaned.replaceAll(/\<(.*):\d+\>(.*)/g, '"$1":"$2",'); // format tags
+                    cleaned = cleaned.replaceAll('"CALL"', '{"CALL"'); // Add curly bracket before each record
+                    cleaned = cleaned.replaceAll('<eor>', '}'); // replace eor tags with closing curly bracket
+                    cleaned = cleaned.replaceAll(/}\s+\{/gm, '},{'); // add comma between brackets
+                    cleaned = cleaned.replaceAll(/,\s+}/gm, '}'); // remove comma after last key/value
+                    cleaned = cleaned.replace('<APP_LoTW_EOF>', ''); // remove EOF tag
+                    cleaned = '[' + cleaned + ']'; // wrap in brackets
+                    const records = JSON.parse(cleaned);
+                    console.log(records);
                 } else {
                     // no records were found
                 }
